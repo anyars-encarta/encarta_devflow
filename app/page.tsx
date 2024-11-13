25fe1f7262ff01a3b8090d3c8197bc0fec625f95
Anyars Yussif <anyarsencarta@gmail.com>
2024-11-13 10:38:34 +0000

    Install tailwindcss-animate and tailwindcss/typography

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,5 @@
 const Home = () => (
-  <h1 className="text-3xl font-black text-light-500">
-    Welcome to the world of Next.js
-  </h1>
+  <h1 className="">Welcome to the world of Next.js</h1>
 );
 
 export default Home;
