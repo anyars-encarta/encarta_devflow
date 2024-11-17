5f509087abf573b4cbbbd62621475c6a36bbd282
Anyars Yussif <anyarsencarta@gmail.com>
2024-11-17 10:15:46 +0000

    Add ThemeProvider in layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -36,7 +36,14 @@ export default function RootLayout({
       <body
         className={`${inter.className} ${spaceGrotesk.variable} antialiased`}
       >
-        <ThemeProvider attribute='class' defaultTheme="system" enableSystem disableTransitionOnChange>{children}</ThemeProvider>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+        </ThemeProvider>
       </body>
     </html>
   );
