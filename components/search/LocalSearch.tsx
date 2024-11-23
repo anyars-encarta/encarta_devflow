3b2d18ca84f24ebc495f8bdf0c1f416c4e558390
Anyars Yussif <anyarsencarta@gmail.com>
2024-11-23 11:54:27 +0000

    Add searchParams and a useState

--- components/search/LocalSearch.tsx
+++ components/search/LocalSearch.tsx
@@ -1,15 +1,16 @@
 "use client";
 
-import React from "react";
+import React, { useState } from "react";
 import { Input } from "../ui/input";
 import Image from "next/image";
+import { useSearchParams } from "next/navigation";
 
 interface LocalSearchProps {
   route: string;
   imgSrc: string;
   placeholder: string;
   otherClasses?: string;
-};
+}
 
 const LocalSearch = ({
   route,
@@ -17,8 +18,15 @@ const LocalSearch = ({
   placeholder,
   otherClasses,
 }: LocalSearchProps) => {
+  const searchParams = useSearchParams();
+  const query = searchParams.get("query") || "";
+
+  const [searchQuery, setSearchQuery] = useState(query);
+
   return (
-    <div className={`${otherClasses} background-light800_darkgradient flex min-h-[56px] grow items-center gap-4 rounded-[10px] px-4`}>
+    <div
+      className={`${otherClasses} background-light800_darkgradient flex min-h-[56px] grow items-center gap-4 rounded-[10px] px-4`}
+    >
       <Image
         src={imgSrc}
         alt="search"
@@ -29,8 +37,8 @@ const LocalSearch = ({
       <Input
         type="text"
         placeholder={placeholder}
-        value=""
-        onChange={(e) => {}}
+        value={searchQuery}
+        onChange={(e) => setSearchQuery(e.target.value)}
         className="paragraph-regular no-focus placeholder text-dark400_light700 border-none shadow-none outline-none"
       ></Input>
     </div>
