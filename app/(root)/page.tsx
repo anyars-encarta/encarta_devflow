76426eebc589d4bc3c809834969c22119245e6ab
Anyars Yussif <anyarsencarta@gmail.com>
2024-11-23 11:40:33 +0000

    Add search input component with an icon

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -2,25 +2,30 @@ import { auth, signOut } from "@/auth";
 import { Button } from "@/components/ui/button";
 import ROUTES from "@/constants/routes";
 import Link from "next/link";
-
+import LocalSearch from "@/components/search/LocalSearch";
 const Home = async () => {
   return (
     <>
       <section className="w-full flex flex-col-reverse sm:flex-row justify-between gap-4 sm:items-center">
         <h1 className="h1-bold text-dark100_light900">All Questions</h1>
 
-        <Button className='primary-gradient min-h-[46px] px-4 py-3 !text-light-900' asChild>
+        <Button
+          className="primary-gradient min-h-[46px] px-4 py-3 !text-light-900"
+          asChild
+        >
           <Link href={ROUTES.ASK_QUESTION}>Ask a Question</Link>
         </Button>
       </section>
-
-      <section className='mt-11'>
-        LocalSearch
+      <section className="mt-11">
+        <LocalSearch
+          route="/"
+          imgSrc="/icons/search.svg"
+          placeholder="Search questions..."
+          otherClasses="flex-1"
+        />
       </section>
-
       HomeFilter
-
-      <div className='mt-10 flex w-full flex-col gap-6'>
+      <div className="mt-10 flex w-full flex-col gap-6">
         <p>Question Card 1</p>
         <p>Question Card 2</p>
         <p>Question Card 3</p>
---
+++ components/search/LocalSearch.tsx
@@ -0,0 +1,40 @@
+"use client";
+
+import React from "react";
+import { Input } from "../ui/input";
+import Image from "next/image";
+
+interface LocalSearchProps {
+  route: string;
+  imgSrc: string;
+  placeholder: string;
+  otherClasses?: string;
+};
+
+const LocalSearch = ({
+  route,
+  imgSrc,
+  placeholder,
+  otherClasses,
+}: LocalSearchProps) => {
+  return (
+    <div className={`${otherClasses} background-light800_darkgradient flex min-h-[56px] grow items-center gap-4 rounded-[10px] px-4`}>
+      <Image
+        src={imgSrc}
+        alt="search"
+        width={24}
+        height={24}
+        className="cursor-pointer"
+      />
+      <Input
+        type="text"
+        placeholder={placeholder}
+        value=""
+        onChange={(e) => {}}
+        className="paragraph-regular no-focus placeholder text-dark400_light700 border-none shadow-none outline-none"
+      ></Input>
+    </div>
+  );
+};
+
+export default LocalSearch;
