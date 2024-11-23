27a0e7a735ffdad172665defe6f4eec60d9704d5
Anyars Yussif <anyarsencarta@gmail.com>
2024-11-23 11:07:31 +0000

    Implement structure of home page

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -6,7 +6,7 @@ import RightSidebar from "@/components/ui/navigation/RightSidebar";
 
 const RootLayout = ({ children }: { children: ReactNode }) => {
   return (
-    <main className='background-light850_dark100 relative'>
+    <main className="background-light850_dark100 relative">
       <Navbar />
 
       <div className="flex">
--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,27 +1,32 @@
 import { auth, signOut } from "@/auth";
 import { Button } from "@/components/ui/button";
 import ROUTES from "@/constants/routes";
+import Link from "next/link";
 
 const Home = async () => {
-  const session = await auth();
-
-  console.log(session);
   return (
     <>
-      <h1 className="h1-bold font-space-grotesk">
-        Welcome to the world of Next.js
-      </h1>
+      <section className="w-full flex flex-col-reverse sm:flex-row justify-between gap-4 sm:items-center">
+        <h1 className="h1-bold text-dark100_light900">All Questions</h1>
 
-      {/* Adds a logout button on the homepage */}
-      {/* <form
-        className="px-10 pt-[100px]"
-        action={async () => {
-          "use server";
-          await signOut({redirectTo: ROUTES.SIGNIN});
-        }}
-      >
-        <Button type="submit">Log out</Button>
-      </form> */}
+        <Button className='primary-gradient min-h-[46px] px-4 py-3 !text-light-900' asChild>
+          <Link href={ROUTES.ASK_QUESTION}>Ask a Question</Link>
+        </Button>
+      </section>
+
+      <section className='mt-11'>
+        LocalSearch
+      </section>
+
+      HomeFilter
+
+      <div className='mt-10 flex w-full flex-col gap-6'>
+        <p>Question Card 1</p>
+        <p>Question Card 2</p>
+        <p>Question Card 3</p>
+        <p>Question Card 4</p>
+        <p>Question Card 5</p>
+      </div>
     </>
   );
 };
