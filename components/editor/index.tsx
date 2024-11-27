3bc37a374229e465ab3f4a084a372e6af9c72881
Anyars Yussif <anyarsencarta@gmail.com>
2024-11-27 10:34:42 +0000

    Implement multiple tag inputs with add and remnove functionalities

--- components/editor/index.tsx
+++ components/editor/index.tsx
@@ -49,7 +49,7 @@ const Editor = ({ value, fieldChange, editorRef, ...props }: EditorProps) => {
       key={resolvedTheme}
       ref={editorRef}
       markdown={value}
-      className="background-light800_dark200 light-border-2 markdown-editor dark-editor w-full border"
+      className="background-light800_dark200 light-border-2 markdown-editor dark-editor grid w-full border"
       onChange={fieldChange}
       plugins={[
         headingsPlugin(),
