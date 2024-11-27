9a2751240781a50f0113d75048177456a07c2d9b
Anyars Yussif <anyarsencarta@gmail.com>
2024-11-27 16:28:12 +0000

    Create User model

---
+++ database/user.model.ts
@@ -0,0 +1,31 @@
+import { model, models, Schema } from "mongoose";
+import { string } from "zod";
+
+export interface IUser {
+    name: string;
+    username: string;
+    email: string;
+    bio?: string;
+    image: string;
+    location?: string;
+    portfolio?: string;
+    reputation?: number;
+};
+
+const UserSchema = new Schema(
+  {
+    name: { type: string, required: true },
+    username: { type: string, required: true },
+    email: { type: string, required: true, unique: true },
+    bio: { type: string },
+    image: { type: string, required: true },
+    location: { type: string },
+    portfolio: { type: string },
+    reputation: { type: Number, default: 0 },
+  },
+  { timestamps: true }
+);
+
+const User = models?.user || model<IUser>("User", UserSchema);
+
+export default User;
