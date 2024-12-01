d0b36435faa155a8b49bce92a42ed604cb9e527d
Anyars Yussif <anyarsencarta@gmail.com>
2024-12-01 10:18:00 +0000

    Implement logging, and correct database connection error

--- lib/mongoose.ts
+++ lib/mongoose.ts
@@ -1,5 +1,7 @@
 import mongoose, { Mongoose } from "mongoose";
 
+import logger from "./logger";
+
 const MONGODB_URI = process.env.MONGODB_URI as string;
 
 if (!MONGODB_URI) {
@@ -24,27 +26,26 @@ if (!cached) {
 
 const dbConnect = async (): Promise<Mongoose> => {
   if (cached.conn) {
+    logger.info("Using existing database connection");
     return cached.conn;
   }
 
   if (!cached.promise) {
-    cached.promise = mongoose.connect(MONGODB_URI, {
-      dbName: "devflow",
-    }).then((result) => {
-        console.log("Connected to MongoDB")
-      return result;
-    }).catch((error) => {
-      console.error("Error connecting to MongoDB", error)
-      throw error
-    })
+    cached.promise = mongoose
+      .connect(MONGODB_URI, {
+        dbName: "devflow",
+      })
+      .then((result) => {
+        logger.info("Connected to MongoDB");
+        return result;
+      })
+      .catch((error) => {
+        logger.error("Error connecting to MongoDB", error);
+        throw error;
+      });
   }
 
-  try {
-    cached.conn = await cached.promise;
-  } catch (e) {
-    cached.promise = null;
-    throw e;
-  }
+  cached.conn = await cached.promise;
 
   return cached.conn;
 };
