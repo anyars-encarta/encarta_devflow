6e51b0b9e34ed4b3f63cd293bf886d017349ee09
Anyars Yussif <anyarsencarta@gmail.com>
2024-12-02 08:42:49 +0000

    Create route handlers

---
+++ app/database.ts
@@ -0,0 +1,64 @@
+const tickets = [
+    {
+        id: 1,
+        name: 'Fix Next.js API route bug',
+        status: 'open',
+        type: 'bug',
+    },
+    {
+        id: 2,
+        name: 'Add a new feature to the app',
+        status: 'open',
+        type: 'bug',
+    },
+    {
+        id: 3,
+        name: 'Refactor the app',
+        status: 'open',
+        type: 'bug',
+    },
+    {
+        id: 4,
+        name: 'Change the Styling of the home page',
+        status: 'open',
+        type: 'bug',
+    },
+    {
+        id: 5,
+        name: 'Add a new feature to the app',
+        status: 'open',
+        type: 'bug',
+    },
+    {
+        id: 6,
+        name: 'Refactor the app',
+        status: 'open',
+        type: 'bug',
+    },
+    {
+        id: 7,
+        name: 'Change the Styling of the home page',
+        status: 'open',
+        type: 'bug',
+    },
+    {
+        id: 8,
+        name: 'Add a new feature to the app',
+        status: 'open',
+        type: 'bug',
+    },
+    {
+        id: 9,
+        name: 'Refactor the app',
+        status: 'open',
+        type: 'bug',
+    },
+    {
+        id: 10,
+        name: 'Change the Styling of the home page',
+        status: 'open',
+        type: 'bug',
+    },
+];
+
+export default tickets;
