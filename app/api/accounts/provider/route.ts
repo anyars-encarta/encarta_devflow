10e324081581bb8eb639fe6cf8dea93bc07e68d3
Anyars Yussif <anyarsencarta@gmail.com>
2024-12-03 12:43:24 +0000

    Implement Account by provider route

---
+++ app/api/accounts/provider/route.ts
@@ -0,0 +1,27 @@
+import { NextResponse } from "next/server";
+
+import Account from "@/database/account.model";
+import handleError from "@/lib/handlers/error";
+import { NotFoundError, ValidationError } from "@/lib/http-error";
+import { AccountSchema } from "@/lib/validations";
+import { APIErrorResponse } from "@/types/global";
+
+export async function POST(request: Request) {
+  const { providerAccountId } = await request.json();
+
+  try {
+    const validatedData = AccountSchema.partial().safeParse({ providerAccountId });
+
+    if (!validatedData.success) {
+      throw new ValidationError(validatedData.error.flatten().fieldErrors);
+    }
+
+    const account = await Account.findOne({ providerAccountId });
+
+    if (!account) throw new NotFoundError("Account");
+
+    return NextResponse.json({ success: true, data: account }, { status: 200 });
+  } catch (e) {
+    return handleError(e, "api") as APIErrorResponse;
+  }
+}
