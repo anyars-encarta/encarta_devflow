96d8ab3176c254d5e4207dcbc7d955874a0a60fb
Anyars Yussif <anyarsencarta@gmail.com>
2025-01-08 17:52:42 +0000

    Impelement edit question

--- constants/routes.ts
+++ constants/routes.ts
@@ -6,6 +6,7 @@ const ROUTES = {
     TAGS: (id: string) => `/tags/${id}`,
     ASK_QUESTION: "/ask-question",
     QUESTIONS: (id: string) => `/questions/${id}`,
+    EDIT_QUESTION: (id: string) => `/questions/${id}/edit`,
     SIGN_IN_WITH_OAUTH: 'signin-with-oauth',
 }
 
