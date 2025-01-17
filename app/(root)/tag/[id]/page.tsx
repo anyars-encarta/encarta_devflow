17b26b97e23e50f15570a056533e0699bee51253
Anyars Yussif <anyarsencarta@gmail.com>
2025-01-17 09:34:34 +0000

    Implement display tag specific questions

--- app/(root)/tag/[id]/page.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const Tag = ({ params }: { params: { id: string } }) => {
-  const { id } = params;
-
-  return <div>Rendering Tag: ${id}</div>;
-};
-
-export default Tag;
