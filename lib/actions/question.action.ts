c6f3f17a585f3d3730c3bdd6942d97eebfbffa58
Anyars Yussif <anyarsencarta@gmail.com>
2025-03-07 16:05:15 +0000

    Implement Votes UI

--- lib/actions/question.action.ts
+++ lib/actions/question.action.ts
@@ -7,6 +7,7 @@ import mongoose, { FilterQuery } from "mongoose";
 import Question, { IQuestionDoc } from "@/database/question.model";
 import TagQuestion from "@/database/tag-question.model";
 import Tag, { ITagDoc } from "@/database/tag.model";
+import { CreateQuestionParams, EditQuestionParams, GetQuestionParams, IncrementViewsParams } from "@/types/action";
 import {
   ActionResponse,
   ErrorResponse,
