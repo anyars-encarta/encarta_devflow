b847880f25bbb48e556ae7413fd65690ddf64e0c
Anyars Yussif <anyarsencarta@gmail.com>
2025-03-07 12:45:16 +0000

    Implement ai api answer route

--- app/(root)/questions/[id]/page.tsx
+++ app/(root)/questions/[id]/page.tsx
@@ -3,6 +3,7 @@ import { redirect } from "next/navigation";
 import { after } from "next/server";
 import React from "react";
 
+import AllAnswers from "@/components/answers/AllAnswers";
 import TagCard from "@/components/cards/TagCard";
 import Preview from "@/components/editor/Preview";
 import AnswerForm from "@/components/forms/AnswerForm";
@@ -13,7 +14,6 @@ import { getAnswers } from "@/lib/actions/answer.action";
 import { getQuestion, incrementViews } from "@/lib/actions/question.action";
 import { formatNumber, getTimeStamp } from "@/lib/utils";
 import { RouteParams } from "@/types/global";
-import AllAnswers from "@/components/answers/AllAnswers";
 
 // import View from "../View"; // imported for First approach for incrementing views
 
--- lib/api.ts
+++ lib/api.ts
@@ -1,8 +1,11 @@
 import ROUTES from "@/constants/routes";
 import { IAccount } from "@/database/account.model";
 import { IUser } from "@/database/user.model";
+import { SignInWithOAuthParams } from "@/types/action";
 
 import { fetchHandler } from "./handlers/fetch";
+import { getAnswers } from "./actions/answer.action";
+import { APIResponse } from "@/types/global";
 
 const API_BASE_URL =
   process.env.NEXT_PUBLIC_API_BASE_URL || "http://localhost:3000/api";
@@ -64,5 +67,12 @@ export const api = {
       fetchHandler(`${API_BASE_URL}/accounts/${id}`, {
         method: "DELETE",
       }),
+  },
+  ai: {
+    getAnswer: (question: string, content: string): APIResponse<string> => 
+      fetchHandler(`${API_BASE_URL}/ai/answers`, {
+      method: "POST",
+      body: JSON.stringify({ question, content }),
+    }),
   },
 };
