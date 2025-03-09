ddc56e373b1bec95a7b71fd327ac23df6173b27b
Anyars Yussif <anyarsencarta@gmail.com>
2025-03-09 09:09:03 +0000

    Correct vote action

--- lib/actions/vote.action.ts
+++ lib/actions/vote.action.ts
@@ -71,7 +71,7 @@ export async function createVote(params: CreateVoteParams): Promise<ActionRespon
   const { targetId, targetType, voteType } = validationResult.params!;
   const userId = validationResult.session?.user?.id;
 
-  if (!userId) handleError(new Error("Unauthorized")) as ErrorResponse;
+  if (!userId) return handleError(new Error("Unauthorized")) as ErrorResponse;
 
   const session = await mongoose.startSession();
   session.startTransaction();
@@ -96,6 +96,11 @@ export async function createVote(params: CreateVoteParams): Promise<ActionRespon
           { voteType },
           { new: true, session }
         );
+        await updateVoteCount(
+          { targetId, targetType, voteType: existingVote.voteType, change: -1 },
+          session
+        );
+
         await updateVoteCount(
           { targetId, targetType, voteType, change: 1 },
           session
