2a58dce9405c4117c609ad5e18e5fe2de52006fe
Anyars Yussif <anyarsencarta@gmail.com>
2025-03-18 16:26:27 +0000

    Remove console.log statement

--- components/ui/navigation/RightSidebar.tsx
+++ components/ui/navigation/RightSidebar.tsx
@@ -9,7 +9,7 @@ import React from "react";
 
 const RightSidebar = async () => {
   const { success, data: hotQuestions, error } = await getHotQuestions();
-console.log("Hot Questions: ", hotQuestions);
+
   return (
     <section className="pt-36 custom-scrollbar background-light900_dark200 light-border sticky right-0 top-0 flex h-screen w-[350px] flex-col gap-6 overflow-y-auto border-l p-6 shadow-light-300 dark:shadow-none max-xl:hidden">
       <div>
