912b95698f5c8399b12459b03f40e3dd2e8f3c5b
Anyars Yussif <anyarsencarta@gmail.com>
2025-03-18 16:55:23 +0000

    Correct line formatting

--- components/ui/navigation/RightSidebar.tsx
+++ components/ui/navigation/RightSidebar.tsx
@@ -11,10 +11,7 @@ import React from "react";
 const RightSidebar = async () => {
   const { success, data: hotQuestions, error } = await getHotQuestions();
   const {
-    success: tagSuccess,
-    data: tags,
-    error: tagError,
-  } = await getTopTags();
+    success: tagSuccess, data: tags, error: tagError } = await getTopTags();
 
   return (
     <section className="pt-36 custom-scrollbar background-light900_dark200 light-border sticky right-0 top-0 flex h-screen w-[350px] flex-col gap-6 overflow-y-auto border-l p-6 shadow-light-300 dark:shadow-none max-xl:hidden">
